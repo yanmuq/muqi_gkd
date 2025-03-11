98971bcaa5a4aff9fb61d0dc9c6e76b43580518f
yanmuq <131337804+yanmuq@users.noreply.github.com>
2025-03-12 02:53:19 +0800

    Update subscription.ts

--- src/subscription.ts
+++ src/subscription.ts
@@ -4,7 +4,7 @@ import categories from './categories';
 import globalGroups from './globalGroups';
 
 export default defineGkdSubscription({
-  id: 717,
+  id: 808,
   name: '九千院的GKD订阅',
   version: 0,
   author: 'jiuqianyuan',
