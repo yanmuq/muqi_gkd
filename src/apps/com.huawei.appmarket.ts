30a48ad18193fec76bee17d6f6a439b87ddffc61
yanmuq <131337804+yanmuq@users.noreply.github.com>
2025-03-12 09:47:46 +0800

    Update com.huawei.appmarket.ts
    删除功能规则

--- src/apps/com.huawei.appmarket.ts
+++ src/apps/com.huawei.appmarket.ts
@@ -35,23 +35,6 @@ export default defineGkdApp({
       ],
     },
     {
-      key: 3,
-      name: '功能类-应用更新页面-点击[查看更多]',
-      fastQuery: true,
-      actionMaximum: 1,
-      resetMatch: 'app',
-      rules: [
-        {
-          activityIds:
-            'com.huawei.appmarket.service.appmgr.view.activity.AppUpdateActivity',
-          matches: '[text="查看更多"]',
-          exampleUrls:
-            'https://m.gkd.li/57941037/e2283b1d-3041-49c5-aca2-7c165c83ce37',
-          snapshotUrls: 'https://i.gkd.li/i/14738587',
-        },
-      ],
-    },
-    {
       key: 4,
       name: '全屏广告-热门应用推荐弹窗',
       desc: '点击[直接进入]',
