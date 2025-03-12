259f1eeaebd996542a8fa26a97155c4c6490e85d
yanmuq <131337804+yanmuq@users.noreply.github.com>
2025-03-12 09:08:32 +0800

    Update subscription.ts

--- src/subscription.ts
+++ src/subscription.ts
@@ -9,7 +9,7 @@ export default defineGkdSubscription({
   version: 0,
   author: 'yanmuqi',
   checkUpdateUrl: './gkd.version.json5',
-  supportUri: 'https://github.com/yanmuq/GKD_subscription',
+  supportUri: 'https://github.com/yanmuq/GKD-yan',
   categories,
   globalGroups,
   apps: await batchImportApps(`${import.meta.dirname}/apps`),
