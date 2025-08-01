c19732204100ab84c63aabded7b31708c1cc5f25
yanmuq <131337804+yanmuq@users.noreply.github.com>
2025-08-01 22:55:59 +0800

    增加美团弹窗规则

--- src/apps/com.sankuai.meituan.ts
+++ src/apps/com.sankuai.meituan.ts
@@ -23,5 +23,43 @@ export default defineGkdApp({
         },
       ],
     },
+    {
+      key: 1,
+      name: '功能类-骑行免密弹窗',
+      fastQuery: true,
+      matchTime: 5000,
+      actionMaximum: 1,
+      resetMatch: 'app',
+      rules: [
+        {
+          key: 1,
+          activityIds:
+            'com.meituan.android.hybridcashier.HybridCashierActivity',
+          matches:
+            '[text="放弃，使用普通支付"][clickable=true][visibleToUser=true]',
+          snapshotUrls: 'https://i.gkd.li/i/21549586',
+          exampleUrls: 'https://e.gkd.li/f45dc954-85bd-4f22-8db7-fb6a9ffc2b0a',
+        },
+      ],
+    },
+    {
+      key: 2,
+      name: '功能类-骑行购卡弹窗',
+      fastQuery: true,
+      matchTime: 5000,
+      actionMaximum: 1,
+      resetMatch: 'app',
+      rules: [
+        {
+          key: 1,
+          activityIds:
+            'com.meituan.msc.modules.container.MSCActivity',
+          matches:
+            '[text="仅支付车费"][clickable=false][visibleToUser=true]',
+          snapshotUrls: 'https://i.gkd.li/i/21549744',
+          exampleUrls: 'https://e.gkd.li/1f4db15c-b109-40ba-bb96-fb756f1ca46b',
+        },
+      ],
+    },
   ],
 });
