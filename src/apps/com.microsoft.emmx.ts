932d69e6bfb402e81a7d9dc0a3c8c10a26bc8f5c
yanmuq <131337804+yanmuq@users.noreply.github.com>
2025-08-03 15:58:53 +0800

    edge通知弹窗

--- src/apps/com.microsoft.emmx.ts
+++ src/apps/com.microsoft.emmx.ts
@@ -5,16 +5,16 @@ export default defineGkdApp({
   name: 'Edge',
   groups: [
     {
-      key: 3,
+      key: 0,
       name: '全屏广告-开启通知弹窗',
-      matchTime: 10000,
+      matchTime: 5000,
       actionMaximum: 1,
       resetMatch: 'app',
       fastQuery: true,
       rules: [
         {
           activityIds: 'org.chromium.chrome.browser.ChromeTabbedActivity',
-          matches: '[text="允许通知"] + [text="不，谢谢"]',
+          matches: 'ViewGroup > Button[text="不，谢谢"]',
           snapshotUrls: 'https://i.gkd.li/i/13646187',
         },
       ],
