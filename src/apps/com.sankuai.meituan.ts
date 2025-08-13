7f838a1f453948d95e22bcb57381c060a88a18be
yanmuq <131337804+yanmuq@users.noreply.github.com>
2025-08-13 21:48:00 +0800

    美团骑行支付弹窗

--- src/apps/com.sankuai.meituan.ts
+++ src/apps/com.sankuai.meituan.ts
@@ -25,39 +25,30 @@ export default defineGkdApp({
     },
     {
       key: 1,
-      name: '功能类-骑行免密弹窗',
-      fastQuery: true,
-      matchTime: 5000,
-      actionMaximum: 1,
-      resetMatch: 'app',
+      name: '功能类-骑行支付弹窗',
       rules: [
         {
           key: 1,
-          activityIds:
-            'com.meituan.android.hybridcashier.HybridCashierActivity',
-          matches:
-            '[text="放弃，使用普通支付"][clickable=true][visibleToUser=true]',
-          snapshotUrls: 'https://i.gkd.li/i/21549586',
+          fastQuery: true,
+          matchTime: 5000,
+          actionMaximum: 2,
+          resetMatch: 'app',
+          activityIds: 'com.meituan.android.hybridcashier.HybridCashierActivity',
+          matches: '@[text^="放弃"] <<n [vid="mil_container"]',
           exampleUrls: 'https://e.gkd.li/f45dc954-85bd-4f22-8db7-fb6a9ffc2b0a',
+          snapshotUrls: 'https://i.gkd.li/i/21549586',
         },
-      ],
-    },
-    {
-      key: 2,
-      name: '功能类-骑行购卡弹窗',
-      fastQuery: true,
-      matchTime: 5000,
-      actionMaximum: 1,
-      resetMatch: 'app',
-      rules: [
         {
-          key: 1,
+          key: 2,
+          fastQuery: true,
+          actionMaximum: 2,
+          resetMatch: 'app',
           activityIds: 'com.meituan.msc.modules.container.MSCActivity',
-          matches: '[text="仅支付车费"][clickable=false][visibleToUser=true]',
+          matches: '[text="仅支付车费"][visibleToUser=true]',
           snapshotUrls: 'https://i.gkd.li/i/21549744',
           exampleUrls: 'https://e.gkd.li/1f4db15c-b109-40ba-bb96-fb756f1ca46b',
         },
       ],
-    },
+    },    
   ],
 });
