29bc715e447ac1875583fc1056f340a435f8644c
yanmuq <131337804+yanmuq@users.noreply.github.com>
2025-12-05 18:32:39 +0800

    bili

--- src/apps/tv.danmaku.bili.ts
+++ src/apps/tv.danmaku.bili.ts
@@ -8,30 +8,31 @@ export default defineGkdApp({
       key: 1,
       name: '青少年弹窗',
       desc: '点击[不再提醒]',
+      fastQuery: true,
       rules: [
         {
-          fastQuery: true,
-          activityIds: [
+          activityIds:
             'com.bilibili.teenagersmode.ui.TeenagersModeDialogActivity',
-          ],
-          matches: ['[id="tv.danmaku.bili:id/button"]', '[text="不再提醒"]'],
+          matches:
+            ['[id="tv.danmaku.bili:id/button"]', '[text="不再提醒"]'],
           snapshotUrls: ['https://i.gkd.li/i/23958306'],
+          exampleUrls: 'https://e.gkd.li/99565077-80e9-4e4f-98e7-33da742e2afd',
         },
       ],
     },
-    //{
-     // key: 2,
-     // name: '全屏广告-推荐弹窗',
-    //  desc: '点击[返回]',
-     // rules: [
-     //   {
-    //      fastQuery: true,
-    //      activityIds: [
-     //       '.MainActivityV2',
-     //     ],
-     //     matches: ['[id="tv.danmaku.bili:id/title"]', '[text="完善信息，获取精准推荐"]'],
-     //     snapshotUrls: ['https://i.gkd.li/i/23958344'],
-     //   },
+    {
+      key: 2,
+      name: '全屏广告-推荐弹窗',
+      desc: '点击[返回]',
+      fastQuery: true,
+      rules: [
+        {
+          fastQuery: true,
+          activityIds: '.MainActivityV2',
+          matches: ['[id="tv.danmaku.bili:id/title"]', '[text^="完善信息"]'],
+          action: "back",
+          snapshotUrls: ['https://i.gkd.li/i/23958344'],
+        },
       ],
     },
   ],
