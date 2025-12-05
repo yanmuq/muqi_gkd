18646f5069263e6c18d5eb239462e63db1d5c129
yanmuq <131337804+yanmuq@users.noreply.github.com>
2025-12-05 18:10:40 +0800

    Update tv.danmaku.bili.ts

--- src/apps/tv.danmaku.bili.ts
+++ src/apps/tv.danmaku.bili.ts
@@ -7,16 +7,31 @@ export default defineGkdApp({
     {
       key: 1,
       name: '青少年弹窗',
-      desc: '点击[我知道了]',
+      desc: '点击[不再提醒]',
       rules: [
         {
           fastQuery: true,
           activityIds: [
             'com.bilibili.teenagersmode.ui.TeenagersModeDialogActivity',
           ],
-          matches: ['[id="tv.danmaku.bili:id/button"]', '[text="我知道了"]'],
-          snapshotUrls: ['https://i.gkd.li/i/21187537'],
+          matches: ['[id="tv.danmaku.bili:id/button"]', '[text="不再提醒"]'],
+          snapshotUrls: ['https://i.gkd.li/i/23958306'],
         },
+      ],
+    },
+    //{
+     // key: 2,
+     // name: '全屏广告-推荐弹窗',
+    //  desc: '点击[返回]',
+     // rules: [
+     //   {
+    //      fastQuery: true,
+    //      activityIds: [
+     //       '.MainActivityV2',
+     //     ],
+     //     matches: ['[id="tv.danmaku.bili:id/title"]', '[text="完善信息，获取精准推荐"]'],
+     //     snapshotUrls: ['https://i.gkd.li/i/23958344'],
+     //   },
       ],
     },
   ],
